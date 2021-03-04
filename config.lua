53b425aa775f30caa46fccc22d42bf87a8a05b9e
Alv <72032190+alvtv@users.noreply.github.com>
2021-03-04 11:52:48 +0000

    New Configs!
    Configurations for the new multi-respawn...

--- config.lua
+++ config.lua
@@ -2,128 +2,97 @@ Config                            = {}
 
 Config.DrawDistance               = 100.0
 
-Config.Marker                     = { type = 27, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false }
+Config.Marker                     = { type = 27, x = 1.5, y = 1.5, z = 0.5, r = 255, g = 0, b = 0, a = 100, rotate = true }
 
-Config.ReviveReward               = 35000  -- revive reward, set to 0 if you don't want it enabled
-Config.AntiCombatLog              = true -- enable anti-combat logging?
-Config.LoadIpl                    = true -- disable if you're using fivem-ipl or other IPL loaders
+Config.AntiCombatLog              = false -- enable anti-combat logging?
 
-Config.Locale = 'en'
+Config.Locale                     = 'en'
 
 local second = 1000
-local minute = 60 * second
+local minute = 30 * second
 
-Config.EarlyRespawnTimer          = 0.3 * minute  -- Time til respawn is available
-Config.BleedoutTimer              = 4 * minute -- Time til the player bleeds out
+Config.EarlyRespawnTimer          = 1 * minute  -- Time til respawn is available
+Config.BleedoutTimer              = 5 * minute -- Time til the player bleeds out --10
 
-Config.EnablePlayerManagement     = false
+Config.EnablePlayerManagement     = true
 
 Config.RemoveWeaponsAfterRPDeath  = true
 Config.RemoveCashAfterRPDeath     = false
 Config.RemoveItemsAfterRPDeath    = false
 
 -- Let the player pay for respawning early, only if he can afford it.
-Config.EarlyRespawnFine           = false
-Config.EarlyRespawnFineAmount     = 3000
+Config.EarlyRespawnFine           = true
+Config.EarlyRespawnFineAmount     = 2500
 
-Config.RespawnPoint = { coords = vector3(-12.11, -1830.72,  25.37), heading = 137.88 }
- 
+Config.RespawnPoint = { coords = vector3(-11.48, -1829.61, 25.39), heading = 139.79 }
+
+Config.RespawnPoint2 = { coords = vector3(218.37, -2592.77, 6.18), heading = 91.76 }
+
 Config.Hospitals = {
 
 	CentralLosSantos = {
 
 		Blip = {
-			coords = vector3(302.36, -586.65, 43.28),
-			sprite = 61,
-			scale  = 1.0,
-			color  = 2
+			coords = vector3(1833.04, 3683.22, 305.23),
+			sprite = 0,
+			scale  = 0,
+			color  = 0
 		},
 
 		AmbulanceActions = {
-			vector3(301.13,-596.37,43.28)
+			vector3(311.99, -597.01, 43.28)
 		},
 
 		Pharmacies = {
-			vector3(307.36,-601.21,43.28)
+			vector3(1838.98, 3682.29, 33.28)
 		},
 
 		Vehicles = {
 			{
-				Spawner = vector3(293.54, -598.09 , 43.29),
-				InsideShop = vector3(446.7, -1355.6, 43.5),
-				Marker = { type = 36, x = 1.0, y = 1.0, z = 1.0, r = 100, g = 50, b = 200, a = 100, rotate = true },
+				Spawner = vector3(297.35, -605.33, 43.33),
+				InsideShop = vector3(994.5925, -3002.594, -40.646),
+				Marker = { type = 36, x = 1.0, y = 1.0, z = 1.0, r = 255, g = 0, b = 0, a = 100, rotate = true },
 				SpawnPoints = {
-					{ coords = vector3(297.91, -605.96 , 43.35), heading = 66.17, radius = 4.0 },
-					{ coords = vector3(294.94 , -611.74, 43.37), heading = 68.66, radius = 4.0 },
-					{ coords = vector3(286.54, -611.62, 43.37 ), heading = 72.63, radius = 6.0 }
+					{ coords = vector3(293.98,-613.18,43.39), heading = 76.21, radius = 4.0 }
 				}
 			}
 		},
 
 		Helicopters = {
 			{
-				Spawner = vector3(317.5, -1449.5, 46.5),
-				InsideShop = vector3(305.6, -1419.7, 41.5),
-				Marker = { type = 34, x = 1.5, y = 1.5, z = 1.5, r = 100, g = 150, b = 150, a = 100, rotate = true },
+				Spawner = vector3(1859.11, 3657.18, 33.96),
+				InsideShop = vector3(1866.13, 3649.89, 35.66),
+				Marker = { type = 34, x = 1.0, y = 1.0, z = 1.0, r = 255, g = 0, b = 0, a = 100, rotate = true },
 				SpawnPoints = {
-					{ coords = vector3(313.5, -1465.1, 46.5), heading = 142.7, radius = 10.0 },
-					{ coords = vector3(299.5, -1453.2, 46.5), heading = 142.7, radius = 10.0 }
+					{ coords = vector3(1866.13, 3649.89, 35.66), heading = 210.7, radius = 4.0 }
 				}
 			}
 		},
 
 		FastTravels = {
-			{
-				From = vector3(294.7, -1448.1, 29.0),
-				To = { coords = vector3(272.8, -1358.8, 23.5), heading = 0.0 },
-				Marker = { type = 1, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false }
-			},
 
 			{
-				From = vector3(275.3, -1361, 23.5),
-				To = { coords = vector3(295.8, -1446.5, 28.9), heading = 0.0 },
-				Marker = { type = 1, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false }
+				From = vector3(1207.7, -1474.67, 34.86),
+				To = { coords = vector3(1201.42, -1484.83, 0.21), heading = 0.0 },
+				Marker = { type = 0, x = 1.0, y = 1.0, z = 0.5, r = 255, g = 0, b = 0, a = 100, rotate = false }
 			},
-
-			{
-				From = vector3(247.3, -1371.5, 23.5),
-				To = { coords = vector3(333.1, -1434.9, 45.5), heading = 138.6 },
-				Marker = { type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false }
-			},
-
-			{
-				From = vector3(335.5, -1432.0, 45.50),
-				To = { coords = vector3(249.1, -1369.6, 23.5), heading = 0.0 },
-				Marker = { type = 1, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false }
-			},
-
-			{
-				From = vector3(234.5, -1373.7, 20.9),
-				To = { coords = vector3(320.9, -1478.6, 28.8), heading = 0.0 },
-				Marker = { type = 1, x = 1.5, y = 1.5, z = 1.0, r = 102, g = 0, b = 102, a = 100, rotate = false }
-			},
-
-			{
-				From = vector3(317.9, -1476.1, 28.9),
-				To = { coords = vector3(238.6, -1368.4, 23.5), heading = 0.0 },
-				Marker = { type = 1, x = 1.5, y = 1.5, z = 1.0, r = 102, g = 0, b = 102, a = 100, rotate = false }
-			}
+			
 		},
 
 		FastTravelsPrompt = {
-		{
-			From = vector3(237.4, -1373.8, 26.0),
-			To = { coords = vector3(251.9, -1363.3, 38.5), heading = 0.0 },
-			Marker = { type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false },
-			Prompt = _U('fast_travel')
-		},
+			{
+				From = vector3(237.4, -1373.8, 26.0),
+				To = { coords = vector3(251.9, -1363.3, 38.5), heading = 0.0 },
+				Marker = { type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false },
+				Prompt = _U('fast_travel')
+			},
 
-		{
-			From = vector3(256.5, -1357.7, 36.0),
-			To = { coords = vector3(235.4, -1372.8, 26.3), heading = 0.0 },
-			Marker = { type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false },
-			Prompt = _U('fast_travel')
-		}
+			{
+				From = vector3(256.5, -1357.7, 36.0),
+				To = { coords = vector3(235.4, -1372.8, 26.3), heading = 0.0 },
+				Marker = { type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false },
+				Prompt = _U('fast_travel')
+			}
 		}
 
 	}
@@ -132,19 +101,23 @@ Config.Hospitals = {
 Config.AuthorizedVehicles = {
 
 	ambulance = {
-		{ model = 'dodgeEMS', label = 'Dodge Charger', price = 10}
+		{ model = 'ambulance', label = 'Ambulance', price = 1},
+		{ model = 'dodgeEMS', label = 'EMS DODGE', price = 1},
 	},
 
 	doctor = {
-		{ model = 'dodgeEMS', label = 'Dodge Charger', price = 10}
+		{ model = 'ambulance', label = 'Ambulance', price = 1},
+		{ model = 'dodgeEMS', label = 'EMS DODGE', price = 1},
 	},
 
 	chief_doctor = {
-		{ model = 'dodgeEMS', label = 'Dodge Charger', price = 10}
+		{ model = 'ambulance', label = 'Ambulance', price = 1},
+		{ model = 'dodgeEMS', label = 'EMS DODGE', price = 1},
 	},
 
 	boss = {
-		{ model = 'dodgeEMS', label = 'Dodge Charger', price = 10}
+		{ model = 'ambulance', label = 'Ambulance', price = 1},
+		{ model = 'dodgeEMS', label = 'EMS DODGE', price = 1},
 	}
 
 }
@@ -154,17 +127,16 @@ Config.AuthorizedHelicopters = {
 	ambulance = {},
 
 	doctor = {
-		{ model = 'buzzard2', label = 'Nagasaki Buzzard', price = 150000 }
-		},
+		{ model = 'polmav', label = 'Maverick', price = 1 }
+	},
 
 	chief_doctor = {
-		{ model = 'buzzard2', label = 'Nagasaki Buzzard', price = 150000 },
-		{ model = 'seasparrow', label = 'Sea Sparrow', price = 300000 }
+		{ model = 'polmav', label = 'Maverick', price = 1 }
 	},
 
 	boss = {
-		{ model = 'buzzard2', label = 'Nagasaki Buzzard', price = 10000 },
-		{ model = 'seasparrow', label = 'Sea Sparrow', price = 250000 }
+		{ model = 'polmav', label = 'Maverick', price = 1},
+		{ model = 'seasparrow', label = 'Sea Sparrow', price = 1 }
 	}
 
 }
